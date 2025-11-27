8c33f9b9a8c8bd8c3ddf09551c8d352bf5b8020b
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-11-27 20:59:46 +0200

    fixed abug in webConfig

--- notify/src/main/java/com/Real/notify/config/WebConfig.java
+++ notify/src/main/java/com/Real/notify/config/WebConfig.java
@@ -1,10 +1,27 @@
 package com.Real.notify.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebConfig {
+public class WebConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        // Messages whose destination starts with /topic will be routed to the broker
+        config.enableSimpleBroker("/topic");
+        // Messages whose destination starts with /app will be routed to @MessageMapping
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // Endpoint for client to connect
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+    }
 }
+
